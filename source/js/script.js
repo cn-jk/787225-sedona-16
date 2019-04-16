ff8d4db0229b81dec023e9e1b93ea94e3d14a8a1
cn-jk <cn_jk@mail.ru>
2019-04-16 03:28:12 -0700

    2.Основное задание--Доработка

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,16 @@ function myMobileMenu() {
   var x = document.getElementById("idMobileMenu");
   var y = document.getElementById("idIconMenu");
 
+  y.classList.remove("main-nav__imageThreeLines");
+  y.classList.remove("main-nav__imageCross");
+
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.background = "green";
+    y.classList.add("main-nav__imageThreeLines");
   } else {
     x.style.display = "block";
     y.style.background = "red";
+    y.classList.add("main-nav__imageCross");
   }
 }
